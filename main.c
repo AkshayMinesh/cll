18ee0570df69d437ce464c59237ab1767132908e
AkshayMinesh <152723120+AkshayMinesh@users.noreply.github.com>
2023-12-31 22:31:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -87,8 +87,8 @@ void showFront(struct Student* head) {
 
 // Function to search for a student record by position
 struct Student* searchByPosition(struct Student* head, int position) {
-    if (head == NULL) {
-        return NULL; // List is empty
+    if (head == NULL || position < 1 || position > nodeCount) {
+        return NULL; // List is empty or invalid position
     }
 
     struct Student* current = head;
